ec5275d7f69115368625b2eb53e7f5baf103dae2
Janyk Steenbeek <janyk@brisp.nl>
2020-05-11 17:29:30 +0200

    Added exception handler for commissions

--- src/Commission.php
+++ src/Commission.php
@@ -2,6 +2,9 @@
 namespace Picqer\BolRetailer;
 
 use GuzzleHttp\Exception\ClientException;
+use Picqer\BolRetailer\Exception\CommissionNotFoundException;
+use Picqer\BolRetailer\Exception\HttpException;
+use Picqer\BolRetailer\Exception\OfferNotFoundException;
 
 class Commission extends Model\Commission
 {
@@ -29,5 +32,26 @@ class Commission extends Model\Commission
         }
 
         return new self(json_decode((string) $response->getBody(), true));
+    }
+
+    private static function handleException(ClientException $e): void
+    {
+        $response = $e->getResponse();
+
+        if ($response && $response->getStatusCode() === 404) {
+            throw new CommissionNotFoundException(
+                json_decode((string) $response->getBody(), true),
+                404,
+                $e
+            );
+        } elseif ($response) {
+            throw new HttpException(
+                json_decode((string) $response->getBody(), true),
+                $response->getStatusCode(),
+                $e
+            );
+        }
+
+        throw $e;
     }
 }
